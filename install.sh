daaf3ee40bd7429abe2ede4764a7218f1a78b700
supermalio <72795376+supermalio@users.noreply.github.com>
2022-03-30 17:40:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 cd /etc/XrayR
 read -p "请输入你的节点ID：" dockerid
-
+echo $dokerid
 sed -i "s|http://127.0.0.1:667|https://gogo.bid|" config.yml
 sed -i "s|123|200423|" config.yml
 sed -i "s|87|$dokerid|" config.yml
