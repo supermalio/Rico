933d640d456eea96a33d4f45063533d43c73acec
supermalio <72795376+supermalio@users.noreply.github.com>
2022-03-30 17:31:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,9 @@
 #/bin/bash
-rpm -q curl || yum install -y curl
-rpm -q wget || yum install -y wget
-suijishu=`cat /dev/urandom | tr -dc '0-9a-z' | head -c 15`
-mkdir /tmp/$suijishu
-cd /tmp/$suijishu
-wget https://raw.githubusercontent.com/961644045/HIM/master/docker-compose.yml
+cd /etc/XrayR
 read -p "请输入你的节点ID：" dockerid
 
-#配置文件
-sed -i "s|HIM|supergogo|" docker-compose.yml
-sed -i "s|sspankey|200423|" docker-compose.yml
-sed -i "s|68|$dockerid|" docker-compose.yml
-sed -i "s|5109|5222|" docker-compose.yml
-sed -i "s|https://sjiuishuhfeufuebfububncissn.tiyunzong.club|http://v985.xyz|" docker-compose.yml
-docker-compose up -d
-docker ps
-echo $suijishu
+sed -i "s|http://127.0.0.1:667|https://gogo.bid|" config.yml
+sed -i "s|123|200423|" config.yml
+sed -i "s|41|dokerid|" config.yml
+
+
