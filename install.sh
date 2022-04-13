756eea7807c763160369bdea6223970450854cf3
supermalio <72795376+supermalio@users.noreply.github.com>
2022-04-13 20:09:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,12 @@ echo $dokerid
 sed -i "s|http://127.0.0.1:667|https://gogo.bid|" config.yml
 sed -i "s|123|200423|" config.yml
 sed -i "s|41|$dockerid|" config.yml
+sed -i 's/EnableProxyProtocol: false/EnableProxyProtocol: true/g' config.yml
 
 cd /etc/systemd/system/
+
 sed -i 17a\Environment="XRAY_VMESS_AEAD_FORCED=false" XrayR.service
+
 systemctl daemon-reload
+
 XrayR restart
