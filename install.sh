ae957cbedb53e2cd92b087eb6b9bf56a98d93313
supermalio <72795376+supermalio@users.noreply.github.com>
2022-09-14 19:07:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sed -i "s|123|200423|" config.yml
 sed -i "s|41|$dockerid|" config.yml
 sed -i 's/EnableProxyProtocol: false/EnableProxyProtocol: true/g' config.yml
 sed -i "s|aaa|2476265480@qq.com|" config.yml
-sed -i "s|bbb|2476265480@qq.com|" config.yml
+sed -i "s|bbb|df8322433670890bd4e8c9ea33bc8de5b0832|" config.yml
 cd /etc/systemd/system/
 
 sed -i 17a\Environment="XRAY_VMESS_AEAD_FORCED=false" XrayR.service
